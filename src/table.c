1732278b898c97772bb1256991fddc728ef47d01
Arguggi <Arguggi@gmail.com>
2012-08-23 17:24:25 +0200

    Add square button

--- src/table.c
+++ src/table.c
@@ -3,8 +3,9 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <math.h>
+#include <float.h>
 #include "table.h"
-#define LABEL_LEN 10
+#define LABEL_LEN 270 
 
 static void square_root(GtkWidget *widget,gpointer data)
 {
@@ -18,11 +19,32 @@ static void square_root(GtkWidget *widget,gpointer data)
     text = atof(gtk_label_get_text(GTK_LABEL(data)));
     if (text > 0){
       result = sqrt(text);
-      sprintf(label_text,"%f",result);
+      sprintf(label_text,"%.3f",result);
       gtk_label_set_text(GTK_LABEL(data),label_text);
     } else {
       gtk_label_set_text(GTK_LABEL(data),"Input is not positive");
     }
+  }
+}
+
+static void square(GtkWidget *widget,gpointer data)
+{
+  gdouble text=0;
+  gchar label_text[LABEL_LEN]="";
+  gint label_lenght=0;
+
+  label_lenght = strlen(gtk_label_get_text(GTK_LABEL(data)));
+
+  if(label_lenght){
+    text = atof(gtk_label_get_text(GTK_LABEL(data)));
+    if(text < sqrt(DBL_MAX)){
+        text = text*text;
+    } else {
+      gtk_label_set_text(GTK_LABEL(data),"Input is too large");
+      return;
+    }
+    sprintf(label_text,"%.3f",text);
+    gtk_label_set_text(GTK_LABEL(data),label_text);
   }
 }
 
@@ -52,7 +74,7 @@ void append_to_label(GtkWidget *widget,gpointer data)
 
 GtkWidget *create_table(void)
 {
-  const gchar table_buttons[4][5] = {{"789()"},{"456+-"},{"123*/"},{"0.SD="}};
+  const gchar table_buttons[4][5] = {{"789()"},{"456+-"},{"123*/"},{"0.SDC"}};
   gint i=0,j=0;
   gchar string[5];
   GtkWidget *table, *button, *label, *frame;
@@ -71,7 +93,7 @@ GtkWidget *create_table(void)
   
   for(j=0;j<4;++j){
     for(i=0;i<5;++i){
-      if(table_buttons[j][i] != 'S' && table_buttons[j][i] != 'D'){
+      if(table_buttons[j][i] != 'S' && table_buttons[j][i] != 'D' && table_buttons[j][i] != 'C'){
         string[0] = table_buttons[j][i];
         string[1] = '\0';
         button = gtk_button_new_with_label(string);
@@ -82,11 +104,16 @@ GtkWidget *create_table(void)
         button = gtk_button_new_with_label(string);
         gtk_table_attach_defaults(GTK_TABLE(table),button,i,i+1,j+1,j+2);
         g_signal_connect(button,"clicked",G_CALLBACK(square_root),label);
-      } else {                                /*Reset button*/
+      } else if (table_buttons[j][i] == 'D'){                                /*Reset button*/
         strncpy(string,"DEL\0",sizeof(gchar)*5);
         button = gtk_button_new_with_label(string);
         gtk_table_attach_defaults(GTK_TABLE(table),button,i,i+1,j+1,j+2);
         g_signal_connect(button,"clicked",G_CALLBACK(cancel_label),label);
+      } else {
+        strncpy(string,"^2\0",sizeof(gchar)*5);
+        button = gtk_button_new_with_label(string);
+        gtk_table_attach_defaults(GTK_TABLE(table),button,i,i+1,j+1,j+2);
+        g_signal_connect(button,"clicked",G_CALLBACK(square),label);
       }
     }
   }
